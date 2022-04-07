300de9e9a562dc90fb402b661d7e02bcbefc20fb
surya <surya@breezeware.net>
2022-04-07 15:02:19 +0530

    version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,56 @@
 module.exports={
-    
-    extends:[
-        'airbnb',
-        'airbnb/hooks',
+
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:jsx-a11y/recommended',
+        'plugin:import/recommended',
+        'plugin:import/typescript',
         'plugin:prettier/recommended',
-        './.eslintrc-base.js'
     ],
+    plugins: ['html', 'prettier', 'react-hooks', 'import', 'simple-import-sort'],
     parser: '@babel/eslint-parser',
     parserOptions: {
       requireConfigFile: false,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true
+      babelOptions: {
+        presets: ['@babel/preset-react'],
       },
-      
-    }
+      ecmaVersion: 'latest', // Use the latest ecmascript standard
+      sourceType: 'module', // Allows using import/export statements
+      ecmaFeatures: {
+        jsx: true, // Enable JSX since we're using React
+      },
+    },
+    settings: {
+      react: {
+        version: 'detect', // Automatically detect the react version
+      },
+    },
+    env: {
+      browser: true, // Enables browser globals like window and document
+      node: true, // Enables Node.js global variables and Node.js scoping.
+      jquery: true,
+      jest: true,
+      amd: true, // Enables require() and define() as global variables as per the amd spec.
+      es6: true,
+    },
+    ignorePatterns: ['node_modules/', '.next/'],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'es5',
+          singleQuote: true,
+          printWidth: 80,
+          semi: false,
+        },
+      ],
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  
+
+
 }
